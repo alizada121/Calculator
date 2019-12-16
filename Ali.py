d8865bfce3259d586313720cab8f322afb1d7f1b
alizada121 <58678445+alizada121@users.noreply.github.com>
2019-12-16 13:33:33 +0400

    leman.py
    it is easy when comand with nubers

--- Ali.py
+++ Ali.py
@@ -9,7 +9,11 @@ def calc(x,y,z):
     return(x*z)
   else:
     return("oops!")
-print("comands:+, - , / , * .")
+  print('''comands:1,2,3,4.
+1-add
+2-substrac
+3-multioly
+4-divide''')
 ask=input().split()
 all=calc(int(ask[0]),ask[1],int(ask[2]))
 print(all)
